493ce887028c6d97c328013d206c2359bbb7ebeb
Jakub Chodziński <xapixowy@gmail.com>
2022-11-30 23:11:53 +0100

    First commit

---
+++ test.js
@@ -0,0 +1,9 @@
+const link =
+   'https://www.komputronik.pl/product/737337/gigabyte-z690-gaming-x-ddr4.html?utm_source=Ceneo&utm_medium=link&utm_campaign=Promo&ceneo_cid=4b97cdb2-26d6-3b7e-7600-64eed064e17d';
+
+const links = {
+   morele: 'https://www.morele.net/plyta-glowna-gigabyte-z690-gaming-x-ddr4-9381882/',
+   xkom: 'https://www.x-kom.pl/p/691355-plyta-glowna-socket-1700-gigabyte-z690-gaming-x-ddr4.html',
+};
+
+console.log(typeof link, typeof links);
