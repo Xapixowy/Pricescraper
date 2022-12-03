ba655002c2baed5fc3416ad6d884a3edbc41e2b1
Jakub Chodziński <xapixowy@gmail.com>
2022-12-03 03:37:12 +0100

    A lot of core functionality is working

--- modules/priceScraper.js
+++ modules/priceScraper.js
@@ -5,58 +5,58 @@ import fs from 'fs';
 
 class Product {
    constructor(name, link) {
-      this.name = '';
+      this.name = name;
       this.links = {
          amazon: undefined,
-         ceneo: undefined,
          euro: undefined,
          komputronik: undefined,
          media: undefined,
          morele: undefined,
+         skapiec: undefined,
          xkom: undefined,
       };
       this.prices = {
          amazon: 0,
-         ceneo: 0,
          euro: 0,
          komputronik: 0,
          media: 0,
          morele: 0,
+         skapiec: 0,
          xkom: 0,
       };
       link && this.setLink(link);
    }
-   setName(name) {}
    setLink(link) {
       if (link === undefined) throw new Error('Link is undefined! You need to enter a link!');
       const type = typeof link;
       if (type === 'string') {
-         if (link.indexOf('www.amazon.pl') >= 0) links.amazon = link;
-         else if (link.indexOf('www.ceneo.pl') >= 0) this.links.ceneo = link;
+         if (link.indexOf('www.amazon.pl') >= 0) this.links.amazon = link;
          else if (link.indexOf('www.euro.pl') >= 0) this.links.euro = link;
-         else if (link.indexOf('www.komputronik.pl') >= 0) this.__links.komputronik = link;
+         else if (link.indexOf('www.komputronik.pl') >= 0) this.links.komputronik = link;
          else if (link.indexOf('www.media.pl') >= 0) this.links.media = link;
          else if (link.indexOf('www.morele.pl') >= 0) this.links.morele = link;
+         else if (link.indexOf('www.skapiec.pl') >= 0) this.links.skapiec = link;
          else if (link.indexOf('www.xkom.pl') >= 0) this.links.xkom = link;
          else throw new Error('Provided link is incorrect! Check if your shop is supported.');
       } else if (type === 'object') {
          this.links.amazon = link.amazon;
-         this.links.ceneo = link.ceneo;
          this.links.euro = link.euro;
          this.links.komputronik = link.komputronik;
          this.links.media = link.media;
          this.links.morele = link.morele;
+         this.links.skapiec = link.skapiec;
          this.links.xkom = link.xkom;
       } else throw new Error('Incorrect type of data! You need to provide string or object.');
+      return true;
    }
    getLink(shop) {
       if (shop === undefined) throw new Error('Shop is undefined! You need to specify a shop!');
       else if (shop === 'amazon') return this.links.amazon;
-      else if (shop === 'ceneo') return this.links.ceneo;
       else if (shop === 'euro') return this.links.euro;
       else if (shop === 'komputronik') return this.links.komputronik;
       else if (shop === 'media') return this.links.media;
       else if (shop === 'morele') return this.links.morele;
+      else if (shop === 'skapiec') return this.links.skapiec;
       else if (shop === 'xkom') return this.links.xkom;
       else throw new Error('Shop with that name is unsupported!');
    }
@@ -65,22 +65,22 @@ class Product {
       const priceFloat = parseFloat(price);
       if (shop === undefined) throw new Error('Shop is undefined! You need to specify a shop!');
       else if (shop === 'amazon') this.prices.amazon = priceFloat;
-      else if (shop === 'ceneo') this.prices.ceneo = priceFloat;
       else if (shop === 'euro') this.prices.euro = priceFloat;
       else if (shop === 'komputronik') this.prices.komputronik = priceFloat;
       else if (shop === 'media') this.prices.media = priceFloat;
       else if (shop === 'morele') this.prices.morele = priceFloat;
+      else if (shop === 'skapiec') this.prices.skapiec = priceFloat;
       else if (shop === 'xkom') this.prices.xkom = priceFloat;
       else throw new Error('Shop with that name is unsupported!');
    }
    getPrice(shop) {
       if (shop === undefined) throw new Error('Shop is undefined! You need to specify a shop!');
       else if (shop === 'amazon') return this.prices.amazon;
-      else if (shop === 'ceneo') return this.prices.ceneo;
       else if (shop === 'euro') return this.prices.euro;
       else if (shop === 'komputronik') return this.prices.komputronik;
       else if (shop === 'media') return this.prices.media;
       else if (shop === 'morele') return this.prices.morele;
+      else if (shop === 'skapiec') return this.prices.skapiec;
       else if (shop === 'xkom') return this.prices.xkom;
       else throw new Error('Shop with that name is unsupported!');
    }
@@ -96,6 +96,226 @@ class Scraper {
    addProduct(product) {
       if (!(product instanceof Product)) throw new Error('Product must be instance of Product class!');
       else this.products.push(product);
+   }
+   removeProduct(id) {
+      if (id === undefined) throw new Error('You must provide product id or name!');
+      else if (typeof id === 'number') this.products.splice(id, 1);
+      else if (typeof id === 'string') {
+         for (let i = 0; i < this.products.length; i++) {
+            this.products[i].name.indexOf(id) >= 0 && this.products.splice(i, 1);
+         }
+      } else throw new Error('Data type is unsupported! Provide an integer or a string.');
+   }
+   async start(productId) {
+      const logError = async function (name, page, err) {
+         const date = new Date();
+         const dateString = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}_${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`;
+         // Create file with page content
+         await fs.writeFile(`./logs/${dateString}_${name}.html`, await page.content(), (err) => {
+            if (err) console.log(err);
+            else console.log(`./logs/${dateString}_${name}.html written successfully!`);
+         });
+         // Create file with error
+         if(err) {
+            await fs.writeFile(`./logs/${dateString}_${name}.txt`, err, (err) => {
+               if (err) console.log(err);
+               else console.log(`./logs/${dateString}_${name}.txt written successfully!`);
+            });
+         }
+         // Create screenshot with page content
+         await page.screenshot({
+            path: `./logs/${dateString}_${name}.jpeg`,
+            fullPage: true,
+         });
+      };
+
+      const getShopPrice = async function (page, product, shop) {
+         if (!product.links[shop]) return false;
+         else {
+            const selectors = {
+               amazon: {
+                  price: {
+                     waitFor: '.a-price .a-offscreen',
+                     get: '.a-price .a-offscreen'
+                  },
+               },
+               euro: {
+                  price: {
+                     waitFor: '.product-price.price-box',
+                     get: '.product-price.price-box'
+                  },
+                  prompt: [
+                     {
+                        waitFor: '#onetrust-consent-sdk',
+                        click: '#onetrust-accept-btn-handler'
+                     }
+                  ],
+               },
+               komputronik: {
+                  price: {
+                     waitFor: '.prices .price .proper',
+                     get: '.prices .price .proper'
+                  }
+               },
+               media: {
+                  price: {
+                     waitFor: '.price-box .prices .main-price',
+                     get: '.price-box .prices .main-price'
+                  }
+               },
+               morele: {
+                  price: {
+                     waitFor: '.product-price',
+                     get: '.product-price'
+                  }
+               },
+               skapiec: {
+                  price: {
+                     waitFor: '.price-history-modal-old-content__main__chart',
+                     get: '.price-history-modal-old-content__details .details-list__item:nth-of-type(1) .price'
+                  },
+                  prompt: [
+                     {
+                        waitFor: '#rasp_cmp',
+                        click: '.cmp-button_button.cmp-intro_acceptAll'
+                     },
+                     {
+                        waitFor: '.gtm_ua_pricehistory',
+                        click: '.gtm_ua_pricehistory'
+                     },
+                     {
+                        waitFor: '.button.gtm_ua_ph_30d',
+                        click: '.button.gtm_ua_ph_30d'
+                     }
+                  ]
+               },
+               xkom: {
+                  price: {
+                     waitFor: '.sc-n4n86h-4.jwVRpW',
+                     get: '.sc-n4n86h-4.jwVRpW'
+                  }
+               },
+            };
+            console.log(`${shop}:`);
+            try {
+               console.log(`1) Loading website...`);
+               const start = new Date();
+               await page.goto(product.links[shop], { waitUntil: 'networkidle0' });
+               const end = new Date();
+               console.log(`1) Finished in ${(end - start) / 1000}s!`);
+               await logError(`${product.name}-${shop}-1`, page);
+            } catch (e) {
+               await logError(`${product.name}-${shop}-1`, page, `${e}`);
+               return false;
+            }
+
+            try {
+               console.log(`2) Waiting for selector...`);
+               const start = new Date();
+               await logError(`${product.name}-${shop}-2-1`, page)
+               if (selectors[shop].prompt) {
+                  for(let prompt of selectors[shop].prompt) {
+                     await page.waitForSelector(prompt.waitFor);
+                     await page.click(prompt.click);
+                  }
+               } 
+               await logError(`${product.name}-${shop}-2-2`, page)
+               const end = new Date();
+               console.log(`2) Finished in ${(end - start) / 1000}s!`);
+            } catch (e) {
+               await logError(`${product.name}-${shop}-2`, page, `${e}`);
+               return false;
+            }
+            
+            let price;
+            try {
+               console.log('3) Evaluating value of selector...');
+               const start = new Date();
+               await page.waitForSelector(selectors[shop].price.waitFor);
+               price = await page.evaluate((sel) => document.querySelector(sel).innerText, selectors[shop].price.get);
+               const end = new Date();
+               console.log(`3) Finished in ${(end - start) / 1000}s!`);
+            } catch (e) {
+               await logError(`${product.name}-${shop}-3`, page, `${e}`);
+               return false;
+            }
+
+            console.log('4) Normalizing price...')
+            console.log(`PRICE: ${price}`)
+            price = price.replaceAll('\n','');
+            price = price.replaceAll('\t','');
+            price = price.replaceAll('zł','');
+            price = price.replaceAll(',','.');
+            product.prices[shop] = parseFloat(price);
+         }
+      };
+      // Provided data check
+      let index;
+      if (productId === undefined) index = -1;         
+      else if (typeof productId === 'number') index = productId;
+      else if (typeof productId === 'string') {
+         for (let i = 0; i < this.products.length; i++) {
+            this.products[i].name.indexOf(productId) >= 0 && (index = i);
+         }
+      } else throw new Error('Data type is unsupported! Provide an integer or a string.');
+
+      // Use stealth plugin in puppeteer
+      puppeteer.use(puppeteerStealth());
+
+      const browser = await puppeteer.launch({
+         executablePath: '/home/xapixowy/.cache/puppeteer/chrome/linux-1056772/chrome-linux/chrome',
+      });
+      const page = await browser.newPage();
+
+      await page.setViewport({
+         width: 1024 + Math.floor(Math.random() * 100),
+         height: 768 + Math.floor(Math.random() * 100),
+         deviceScaleFactor: 1,
+         hasTouch: false,
+         isLandscape: false,
+         isMobile: false,
+      });
+      // await page.setDefaultNavigationTimeout(0);
+      // await page.setDefaultTimeout(0);
+
+      // Random user agent
+      const userAgent = randomUseragent.getRandom(function (ua) {
+         return (ua.osName === 'Windows' && parseFloat(ua.browserVersion) >=20);
+      });
+      await page.setUserAgent(userAgent);
+      console.log('USERAGENT:', userAgent);
+
+      // Abort fonts and images in page loading
+      await page.setRequestInterception(true);
+      page.on('request', (req) => {
+         if (req.resourceType() == 'font' || req.resourceType() == 'image')
+            req.abort();
+         else req.continue();
+      });
+
+      console.log('INDEX: ', index);
+
+      if (index > -1) {
+         const product = this.products[index];
+         if (!product) throw new Error('Product is undefined!');
+         else for (let shopName in product.links) await getShopPrice(page, product, shopName);
+      } else {
+         for (let product of this.products) {
+            if (!product.name) break; // If product doesn't have name, don't evaluate its prices
+            else {
+               for (let linkName in product.links) {
+                  const link = product.links[linkName];
+                  console.log(linkName, link);
+                  if (!link) break;
+                  else {
+                    
+                  }
+               }
+            }
+         }
+      }
+
+      await browser.close();
    }
 }
 
--- test.js
+++ test.js
@@ -3,21 +3,20 @@ import { Product, Scraper } from './modules/priceScraper.js';
 const links = {
    amazon:
       'https://www.amazon.pl/Straight-Platinum-modulowy-platyna-zasilacz/dp/B083T9STQS/ref=sr_1_1?__mk_pl_PL=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=1V0GKSXE66W1L&keywords=be%2Bquiet!%2BStraight%2BPower%2B11%2B850W%2B80%2BPlus%2BPlatinum&qid=1669933060&qu=eyJxc2MiOiIwLjg5IiwicXNhIjoiMC4wMCIsInFzcCI6IjAuMDAifQ%3D%3D&sprefix=be%2Bquiet%2Bstraight%2Bpower%2B11%2B850w%2B80%2Bplus%2Bplatinum%2Caps%2C247&sr=8-1&th=1',
-   ceneo: 'https://www.ceneo.pl/90571442',
+   skapiec: 'https://www.skapiec.pl/site/cat/13/comp/897114162#ceny',
    euro: 'https://www.euro.com.pl/zasilacze-do-komputerow-pc/be-quiet-zasilacz-bequiet-straight-power-11-850w.bhtml',
    media: 'https://www.mediaexpert.pl/komputery-i-tablety/podzespoly-komputerowe/zasilacze/zasilacz-be-quiet-straight-power-11-850w-80-plus-platinum',
    morele: 'https://www.morele.net/zasilacz-be-quiet-straight-power-11-850w-bn308-6470639/',
    xkom: 'https://www.x-kom.pl/p/540597-zasilacz-do-komputera-be-quiet-straight-power-11-850w-80-plus-platinum.html',
 };
 
-const product = new Product('be quiet! Straight Power 11 850W 80+ Platinum', links);
+const product1 = new Product('be quiet! Straight Power 11 850W 80+ Platinum', links);
 const fakeProduct = 'be quiet! Straight Power 11 850W 80+ Platinum';
 const scraper = new Scraper();
 
-product.setLink(links);
+scraper.addProduct(product1);
+await scraper.start(0);
 
-scraper.addProduct(product);
-// for (const producto of scraper.getProducts()) console.log(producto.name);
+for (let product of scraper.getProducts()) console.log(product.prices);
 
-console.log(product);
-// console.log(scraper);
+console.log(scraper.getProducts());
